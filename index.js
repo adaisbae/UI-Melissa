3945ff6e05d0cd6a38acf6bca443eba0022c74c7
adaisbae <melissajoneswrites@gmail.com>
2019-06-28 07:39:35 -0400

    commit before pull request. event listener is not working but need something to submit while I keep working on it

--- index.js
+++ index.js
@@ -11,6 +11,6 @@
 
 
 
-let photos = document.querySelectorAll('.photo-box img');;
-Array.from(photos).forEach(photo => photo.addEventListener("mouseenter", function(event) 
-{event.target.style.border = "#3D85C6";}));
+let photos = document.querySelectorAll('.photo-box img');
+Array.from(photos).map(photo => photo.addEventListener("click", function(event) 
+{event.target.style.border = "thin solid #3D85C6";}));
