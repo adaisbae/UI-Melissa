8aa25857c4b21c37bfeaf7234c193e72582c4465
adaisbae <melissajoneswrites@gmail.com>
2019-06-28 06:52:43 -0400

    started working on adding event listeners

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-axios.get(“https://donor-manage-bw.herokuapp.com/api/donations/total”).then((response) => { response.data }).response.data contains {total: 1000}
-
-
 
 // Event Listeners
 
+
+// let photos = document.querySelectorAll('.photo-box img');
+// Array.from(photos).forEach(photo => photo.addEventListener('click', highlightPhotos));
+
+// function highlightPhotos(){
+//     event.target.style.border = 'thick solid @nav-text';
+// }
+
+
+let photos = document.querySelectorAll('.photo-box img');;
+Array.from(photos).forEach(photo => photo.addEventListener("mouseenter", function(event) 
+{event.target.style.border = "#3D85C6";}));
